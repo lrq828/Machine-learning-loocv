4240c33c6cffbb09876c5bba37e6aa1c5acb849f
lrq828 <105230694+lrq828@users.noreply.github.com>
2023-01-11 12:09:23 +0800

    Update main.R

--- main.R
+++ main.R
@@ -22,6 +22,7 @@ rm(list = ls())
   library(ggRandomForests)
   source("fun/CoxBoost.R");source("fun/Enet.R");source("fun/GBM.R");source("fun/plsRcox.R")
   source("fun/RSF.R");source("fun/StepCox.R");source("fun/superPC.R");source("fun/Survivalsvm.R")
+  source("cindex.R")
   
   suppressWarnings(suppressMessages(future::plan("multiprocess",workers = 5)))
 }
@@ -29,18 +30,21 @@ rm(list = ls())
 #### 准备工作 ####
 ##################################
 {
+  suppressWarnings(suppressMessages(future::plan("multiprocess",workers = 6)))
   setwd("E:\\CA\\machine learning\\circle")
   
-  result <- data.frame()
+  result <- data.frame();cc <- data.frame()
   #读取数据
-  data <- read.delim("matrix.txt")
+  data <- read.delim("test.txt")
   rownames(data)=data[,1]
   data=data[,-1]
-  
+
   #划分训练集和测试集
   size <- createDataPartition(data$OS.time,p=0.7,list = F)  # 随机选择70%的数据作为Train data
   est_dd <- data[size,]
   val_dd_list <- data[-size,]
+  # est_dd <- read.delim("matrix.txt")
+  # val_dd_list <- read.delim("test.txt")
   
   rf_nodesize <- 15
   seed <- 2022
@@ -58,8 +62,7 @@ for (i in c(seq(1,8,1))) {
     firstfun <- funlists[[i]]
     for (j in c(seq(1,2,1))) {
       ll <- firstfun(est_dd,val_dd_list,dirnames[j])
-      fit <- ll[1];RS <- unlist(ll[2])
-      est_dd2 <- as.data.frame(ll[3]);val_dd_list2 <- as.data.frame(ll[4])
+      est_dd2 <- as.data.frame(ll[2]);val_dd_list2 <- as.data.frame(ll[3])
       ############## 第二层算法########################
       for (k in c(seq(1,8,1))) {
         # Enet
@@ -67,12 +70,8 @@ for (i in c(seq(1,8,1))) {
           secondfun <- funlists[[k]]
           for (m in c(seq(1,11,1))) {
             lll <- secondfun(est_dd2,val_dd_list2,alphalist[m])
-            fit <- lll[1];RS <- unlist(lll[2])
-            
-            rs=as.data.frame(cbind(val_dd_list2[,1:2],RS))
-            aa=coxph(Surv(OS.time,OS)~RS,rs)
-            cindex=as.numeric(summary(aa)$concordance[1])
-            cc <- data.frame(cindex)%>%rownames_to_column('ID')
+            train_cindex <- unlist(lll[4]);test_cindex <- unlist(lll[5])
+            cc <- as.data.frame(cbind(train_cindex,test_cindex))
             #模型结果
             cc$Model <- paste0(names[i],'(',dirnames[j],')','+',names[k],'(alpha=',alphalist[m],')')
             result <- rbind(result,cc)
@@ -82,12 +81,8 @@ for (i in c(seq(1,8,1))) {
         else if(k %in% c(3,4)){
           secondfun <- funlists[[k]]
           lll <- secondfun(est_dd2,val_dd_list2)
-          fit <- lll[1];RS <- unlist(lll[2])
-          
-          rs=as.data.frame(cbind(val_dd_list2[,1:2],RS))
-          aa=coxph(Surv(OS.time,OS)~RS,rs)
-          cindex=as.numeric(summary(aa)$concordance[1])
-          cc <- data.frame(cindex)%>%rownames_to_column('ID')
+          train_cindex <- unlist(lll[4]);test_cindex <- unlist(lll[5])
+          cc <- as.data.frame(cbind(train_cindex,test_cindex))
           #模型结果
           cc$Model <- paste0(names[i],'(',dirnames[j],')','+',names[k])
           result <- rbind(result,cc)
@@ -96,22 +91,16 @@ for (i in c(seq(1,8,1))) {
         else if(k %in% c(seq(5,8,1))){ 
           secondfun <- funlists[[k]]
           lll <- secondfun(est_dd2,val_dd_list2)
-          fit <- lll[1];RS <- unlist(lll[2])
-          
-          rs=as.data.frame(cbind(val_dd_list2[,1:2],RS))
-          aa=coxph(Surv(OS.time,OS)~RS,rs)
-          cindex=as.numeric(summary(aa)$concordance[1])
-          cc <- data.frame(cindex)%>%rownames_to_column('ID')
+          train_cindex <- unlist(lll[2]);test_cindex <- unlist(lll[3])
+          cc <- as.data.frame(cbind(train_cindex,test_cindex))
           #模型结果
           cc$Model <- paste0(names[i],'(',dirnames[j],')','+',names[k])
           result <- rbind(result,cc)
         }
         #null
         else if(k==i){
-          rs=as.data.frame(cbind(val_dd_list[,1:2],RS))
-          aa=coxph(Surv(OS.time,OS)~RS,rs)
-          cindex=as.numeric(summary(aa)$concordance[1])
-          cc <- data.frame(cindex)%>%rownames_to_column('ID')
+          train_cindex <- unlist(ll[4]);test_cindex <- unlist(ll[5])
+          cc <- as.data.frame(cbind(train_cindex,test_cindex))
           #模型结果
           cc$Model <- paste0(names[i],'(',dirnames[j],')')
           result <- rbind(result,cc)
@@ -124,8 +113,7 @@ for (i in c(seq(1,8,1))) {
     firstfun <- funlists[[i]]
     for (j in c(seq(1,11,1))) {
       ll <- firstfun(est_dd,val_dd_list,alphalist[j])
-      fit <- ll[1];RS <- unlist(ll[2])
-      est_dd2 <- as.data.frame(ll[3]);val_dd_list2 <- as.data.frame(ll[4])
+      est_dd2 <- as.data.frame(ll[2]);val_dd_list2 <- as.data.frame(ll[3])
       ############### 第二层算法##################
       for (k in c(seq(1,8,1))) {
         # StepCox
@@ -133,12 +121,8 @@ for (i in c(seq(1,8,1))) {
           secondfun <- funlists[[k]]
           for (m in c(seq(1,3,1))) {
             lll <- secondfun(est_dd2,val_dd_list2,dirnames[m])
-            fit <- lll[1];RS <- unlist(lll[2])
-            
-            rs=as.data.frame(cbind(val_dd_list2[,1:2],RS))
-            aa=coxph(Surv(OS.time,OS)~RS,rs)
-            cindex=as.numeric(summary(aa)$concordance[1])
-            cc <- data.frame(cindex)%>%rownames_to_column('ID')
+            train_cindex <- unlist(lll[4]);test_cindex <- unlist(lll[5])
+            cc <- as.data.frame(cbind(train_cindex,test_cindex))
             #模型结果
             cc$Model <- paste0(names[i],'(alpha=',alphalist[j],')','+',names[k],'(',dirnames[m],')')
             result <- rbind(result,cc)
@@ -148,12 +132,8 @@ for (i in c(seq(1,8,1))) {
         else if(k %in% c(3,4)){
           secondfun <- funlists[[k]]
           lll <- secondfun(est_dd2,val_dd_list2)
-          fit <- lll[1];RS <- unlist(lll[2])
-          
-          rs=as.data.frame(cbind(val_dd_list2[,1:2],RS))
-          aa=coxph(Surv(OS.time,OS)~RS,rs)
-          cindex=as.numeric(summary(aa)$concordance[1])
-          cc <- data.frame(cindex)%>%rownames_to_column('ID')
+          train_cindex <- unlist(lll[4]);test_cindex <- unlist(lll[5])
+          cc <- as.data.frame(cbind(train_cindex,test_cindex))
           #模型结果
           cc$Model <- paste0(names[i],'(alpha=',alphalist[j],')','+',names[k])
           result <- rbind(result,cc)
@@ -162,22 +142,16 @@ for (i in c(seq(1,8,1))) {
         else if(k %in% c(seq(5,8,1))){ 
           secondfun <- funlists[[k]]
           lll <- secondfun(est_dd2,val_dd_list2)
-          fit <- lll[1];RS <- unlist(lll[2])
-          
-          rs=as.data.frame(cbind(val_dd_list2[,1:2],RS))
-          aa=coxph(Surv(OS.time,OS)~RS,rs)
-          cindex=as.numeric(summary(aa)$concordance[1])
-          cc <- data.frame(cindex)%>%rownames_to_column('ID')
+          train_cindex <- unlist(lll[2]);test_cindex <- unlist(lll[3])
+          cc <- as.data.frame(cbind(train_cindex,test_cindex))
           #模型结果
           cc$Model <- paste0(names[i],'(alpha=',alphalist[j],')','+',names[k])
           result <- rbind(result,cc)
         }
         #null
         else if(k==i){
-          rs=as.data.frame(cbind(val_dd_list[,1:2],RS))
-          aa=coxph(Surv(OS.time,OS)~RS,rs)
-          cindex=as.numeric(summary(aa)$concordance[1])
-          cc <- data.frame(cindex)%>%rownames_to_column('ID')
+          train_cindex <- unlist(ll[4]);test_cindex <- unlist(ll[5])
+          cc <- as.data.frame(cbind(train_cindex,test_cindex))
           #模型结果
           cc$Model <- paste0(names[i],'(alpha=',alphalist[j],')')
           result <- rbind(result,cc)
@@ -189,8 +163,7 @@ for (i in c(seq(1,8,1))) {
   else if(i %in% c(3,4)){ 
     firstfun <- funlists[[i]]
     ll <- firstfun(est_dd,val_dd_list)
-    fit <- ll[1];RS <- unlist(ll[2])
-    est_dd2 <- as.data.frame(ll[3]);val_dd_list2 <- as.data.frame(ll[4])
+    est_dd2 <- as.data.frame(ll[2]);val_dd_list2 <- as.data.frame(ll[3])
     ##########第二层算法#########
     for (k in c(seq(1,8,1))) {
       # StepCox
@@ -198,12 +171,8 @@ for (i in c(seq(1,8,1))) {
         secondfun <- funlists[[k]]
         for (m in c(seq(1,3,1))) {
           lll <- secondfun(est_dd2,val_dd_list2,dirnames[m])
-          fit <- lll[1];RS <- unlist(lll[2])
-          
-          rs=as.data.frame(cbind(val_dd_list2[,1:2],RS))
-          aa=coxph(Surv(OS.time,OS)~RS,rs)
-          cindex=as.numeric(summary(aa)$concordance[1])
-          cc <- data.frame(cindex)%>%rownames_to_column('ID')
+          train_cindex <- unlist(lll[4]);test_cindex <- unlist(lll[5])
+          cc <- as.data.frame(cbind(train_cindex,test_cindex))
           #模型结果
           cc$Model <- paste0(names[i],'+',names[k],'(',dirnames[m],')')
           result <- rbind(result,cc)
@@ -214,12 +183,8 @@ for (i in c(seq(1,8,1))) {
         secondfun <- funlists[[k]]
         for (m in c(seq(1,11,1))) {
           lll <- secondfun(est_dd2,val_dd_list2,alphalist[m])
-          fit <- lll[1];RS <- unlist(lll[2])
-          
-          rs=as.data.frame(cbind(val_dd_list2[,1:2],RS))
-          aa=coxph(Surv(OS.time,OS)~RS,rs)
-          cindex=as.numeric(summary(aa)$concordance[1])
-          cc <- data.frame(cindex)%>%rownames_to_column('ID')
+          train_cindex <- unlist(lll[4]);test_cindex <- unlist(lll[5])
+          cc <- as.data.frame(cbind(train_cindex,test_cindex))
           #模型结果
           cc$Model <- paste0(names[i],'+',names[k],'(alpha=',alphalist[m],')')
           result <- rbind(result,cc)
@@ -229,21 +194,35 @@ for (i in c(seq(1,8,1))) {
       else if(k %in% c(seq(5,8,1))){ 
         secondfun <- funlists[[k]]
         lll <- secondfun(est_dd2,val_dd_list2)
-        fit <- lll[1];RS <- unlist(lll[2])
-        
-        rs=as.data.frame(cbind(val_dd_list2[,1:2],RS))
-        aa=coxph(Surv(OS.time,OS)~RS,rs)
-        cindex=as.numeric(summary(aa)$concordance[1])
-        cc <- data.frame(cindex)%>%rownames_to_column('ID')
+        train_cindex <- unlist(lll[2]);test_cindex <- unlist(lll[3])
+        cc <- as.data.frame(cbind(train_cindex,test_cindex))
         #模型结果
         cc$Model <- paste0(names[i],'+',names[k])
         result <- rbind(result,cc)
       }
+      # RSF
+      else if(k == 3 && i != 3){
+        secondfun <- funlists[[k]]
+        lll <- secondfun(est_dd2,val_dd_list2)
+        train_cindex <- unlist(lll[4]);test_cindex <- unlist(lll[5])
+        cc <- as.data.frame(cbind(train_cindex,test_cindex))
+        #模型结果
+        cc$Model <- paste0(names[i],'(alpha=',alphalist[j],')','+',names[k])
+        result <- rbind(result,cc)
+      }
+      # myCoxBoost
+      else if(k == 4 && i != 4){
+        secondfun <- funlists[[k]]
+        lll <- secondfun(est_dd2,val_dd_list2)
+        train_cindex <- unlist(lll[4]);test_cindex <- unlist(lll[5])
+        cc <- as.data.frame(cbind(train_cindex,test_cindex))
+        #模型结果
+        cc$Model <- paste0(names[i],'(alpha=',alphalist[j],')','+',names[k])
+        result <- rbind(result,cc)
+      }
       else if(k==i){
-        rs=as.data.frame(cbind(val_dd_list[,1:2],RS))
-        aa=coxph(Surv(OS.time,OS)~RS,rs)
-        cindex=as.numeric(summary(aa)$concordance[1])
-        cc <- data.frame(cindex)%>%rownames_to_column('ID')
+        train_cindex <- unlist(ll[4]);test_cindex <- unlist(ll[5])
+        cc <- as.data.frame(cbind(train_cindex,test_cindex))
         #模型结果
         cc$Model <- paste0(names[i])
         result <- rbind(result,cc)
@@ -254,12 +233,8 @@ for (i in c(seq(1,8,1))) {
   else if(i %in% c(seq(5,8,1))){
     firstfun <- funlists[[i]]
     ll <- firstfun(est_dd,val_dd_list)
-    fit <- ll[1];RS <- unlist(ll[2])
-    
-    rs=as.data.frame(cbind(val_dd_list[,1:2],RS))
-    aa=coxph(Surv(OS.time,OS)~RS,rs)
-    cindex=as.numeric(summary(aa)$concordance[1])
-    cc <- data.frame(cindex)%>%rownames_to_column('ID')
+    train_cindex <- unlist(ll[2]);test_cindex <- unlist(ll[3])
+    cc <- as.data.frame(cbind(train_cindex,test_cindex))
     #模型结果
     cc$Model <- paste0(names[i])
     result <- rbind(result,cc)
@@ -267,10 +242,3 @@ for (i in c(seq(1,8,1))) {
 }
 
 write.csv(result,"result.csv")
-
-
-
-
-
-
-
